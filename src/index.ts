6f13b80e66a248a32523ad25a16b93c8fc5bd961
zakariaemse <81692956+zakariaemse@users.noreply.github.com>
2021-06-14 21:22:47 +0200

    maj

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,29 @@
 // You can write your WorkAdventure script here, if any.
 // The "WA" global object is available from anywhere.
 
-console.log('Script started successfully');
-WA.openCoWebSite('https://workadventu.re');
+console.log('Script started successfully ! ');
+
+WA.sendChatMessage("Bienvenue sur WorkAdventures, n'hésitez pas à soliciter le staff pour toutes vos questions. Nous esperons que vous allez passer un super moment en notre compagnie.\n Vous pouvez fermer cette fenêtre.", "L'équipe HTH");
+
+
+let helloWorldPopup: any;
+
+// Open the popup when we enter a given zone
+helloWorldPopup = WA.onEnterZone('sport1', () => {
+    WA.openPopup("popup_sport1", 'Bienvenue dans la salle Teddy Riner !', [{
+        label: "Close",
+        className: "primary",
+        callback: (popup) => {
+            // Close the popup when the "Close" button is pressed.
+            popup.close();
+        }
+    });
+}]);
+
+// Close the popup when we leave the zone.
+WA.onLeaveZone('sport1', () => {
+    helloWorldPopup.close();
+});
+
+
+
