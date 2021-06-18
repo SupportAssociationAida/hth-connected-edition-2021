affe5df0926323470104cf5db87e323263e56e9c
zakariaemse <81692956+zakariaemse@users.noreply.github.com>
2021-06-18 21:31:42 +0200

    musique

--- script.js
+++ script.js
@@ -7,23 +7,23 @@ console.log('Script started successfully !');
 WA.sendChatMessage("Bienvenue sur WorkAdventures, n'hésitez pas à soliciter le staff pour toutes vos questions. Nous esperons que vous allez passer un super moment en notre compagnie.\n Vous pouvez fermer cette fenêtre.", "L'équipe HTH"); 
 
 
-//  var config = {
-//     volume : 0.5,
-//     loop : false,
-//     rate : 1,
-//     detune : 1,
-//     delay : 0,
-//     seek : 0,
-//     mute : false
-// }
+ var config = {
+    volume : 0.5,
+    loop : false,
+    rate : 1,
+    detune : 1,
+    delay : 0,
+    seek : 0,
+    mute : false
+}
 
 
-// var mySound = WA.loadSound("sons/yiruma.mp3"); 
+ var mySound = WA.loadSound("sons/yiruma.mp3"); 
 
-// WA.onEnterZone('piano', () => {
-//   mySound.play(config);
-// })
+WA.onEnterZone('piano', () => {
+  mySound.play(config);
+})
   
-// WA.onLeaveZone('piano', () => {
-//   mySound.stop(); 
-// });
+WA.onLeaveZone('piano', () => {
+  mySound.stop(); 
+});
